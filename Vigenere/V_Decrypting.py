09decc12f38c9e6a00cdbb8384064eeb47012bc9
Haroun <harooonabidi@gmail.com>
2023-03-13 21:58:10 +0100

    Finish

--- Vigenere/V_Decrypting.py
+++ Vigenere/V_Decrypting.py
@@ -1,25 +1,39 @@
-# ciphertext: rilew fretj qgzrv uperr vdajq grwue qrwue qrszh clcer nqjlc dstqv aluan ouedm qbqgr mhwqh etgqz yh
-# key: raymond
+# rilew fretj qgzrv uperr vdajq grwue qrszh clcer nqjlc dstqv aluan ouedm qbqgr mhwqh etgqz yh
 
-def vigenere_decrypt(text, key) :
-    
+def vigenere_decrypt(cipher, key) :
+  
+  spaces_indexes = []
+  
+  # put space indexes in array
+  for i in range(len(cipher)) :
+    if cipher[i] == " " :
+      spaces_indexes.append(i)
+  
   decrypted = ""
+  
+  # remove spaces from the cipher
+  cipher_ws = cipher.replace(" ", "")
 
-  for i in range(len(text)) :
+  # decrypt cipher without spaces
+  for i in range(len(cipher_ws)) :
 
-    text_p = ord(text[i])
+    cipher_p = ord(cipher_ws[i])
     key_p = ord(key[i % len(key)])
     
-    if text[i] == " " :
-      decrypted += " "
-    else :
-      decrypted += chr((text_p - key_p) % 26 + 97)
+    decrypted += chr((cipher_p - key_p + 26) % 26 + 97)
 
+  # check if i is in space indexes array and add a space to encrypted text 
+  for i in spaces_indexes :
+    decrypted = decrypted[:i] + " " + decrypted[i:]
+  
   return decrypted
 
-text = input("Enter a text to decrypt: ")
-key = input("Decryption key: ")
+# cipher = input("Enter a cipher to decrypt: ")
+cipher = "rilew fretj qgzrv uperr vdajq grwue qrszh clcer nqjlc dstqv aluan ouedm qbqgr mhwqh etgqz yh"
+
+# key = input("Decryption key: ")
+key = "raymond"
 
 print("\n-------------------------")
-print("Plaintext: " + vigenere_decrypt(text, key))
+print("Plaintext: " + vigenere_decrypt(cipher, key))
 print("-------------------------")
--- Vigenere/V_Encrypting.py
+++ Vigenere/V_Encrypting.py
@@ -1,21 +1,37 @@
 def vigenere_encrypt(text, key) :
 
+	spaces_indexes = []
+
+  # put space indexes in array
+	for i in range(len(text)):
+		if text[i] == " ":
+			spaces_indexes.append(i)
+
 	encrypted = ""
 	
-	for i in range(len(text)) :
+	# remove spaces from the text
+	text_ws = text.replace(" ", "")
+
+  # encrypt text without spaces
+	for i in range(len(text_ws)) :
 		
-		text_p = ord(text[i]) - 97
+		text_p = ord(text_ws[i]) - 97
 		key_p = ord(key[i % len(key)]) - 97
 
-		if text[i] == " " :
-			encrypted += " "
-		else :
-			encrypted += chr((text_p + key_p) % 26 + 97)
+		encrypted += chr((text_p + key_p) % 26 + 97)
+
+	
+	# check if i is in space indexes array and add a space to encrypted text
+	for i in spaces_indexes :
+		encrypted = encrypted[:i] + " " + encrypted[i:]
 
 	return encrypted
 
-text = input("Enter a text to encrypt: ")
-key = input("Encryption key: ")
+# text = input("Enter a text to encrypt: ")
+text = "ainsi sontl esmoe ursde smale setde sfeme llesd ansle regne anima ldefa conpr ovide ntiel le"
+
+# key = input("Encryption key: ")
+key = "raymond"
 
 print("\n-------------------------")
 print("Ciphertext: " + vigenere_encrypt(text, key))
